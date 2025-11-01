da4b606465fafcbb976a465fb808547f296ab4ea
Ishaka Ericsson <eericsson048@gmail.com>
2025-11-01 08:42:22 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,10 +1,25 @@
-import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
+import {
+  createLocalizedPathnamesNavigation,
+  Pathnames
+} from 'next-intl/navigation';
  
 export const locales = ['en', 'fr'] as const;
 export const localePrefix = 'always'; // Default
  
+// The `pathnames` object holds pairs of internal
+// and external paths, separated by locale.
+export const pathnames = {
+  // If all locales use the same pathnames, a single
+  // external path can be used for all locales.
+  '/': '/',
+  '/login': '/login',
+  '/dashboard': '/dashboard',
+ 
+} satisfies Pathnames<typeof locales>;
+ 
 export const {Link, redirect, usePathname, useRouter} =
   createLocalizedPathnamesNavigation({
     locales,
-    localePrefix
+    localePrefix,
+    pathnames
   });

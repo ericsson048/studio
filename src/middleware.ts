df754f62195ba686c4b048728c57caceb6a71c51
Ishaka Ericsson <eericsson048@gmail.com>
2025-11-01 10:28:25 +0000

    This page could not be found. corrige /en

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales, localePrefix } from './navigation';
+import { locales, localePrefix } from '../navigation';
  
 export default createMiddleware({
   defaultLocale: 'en',
@@ -9,12 +9,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    // However, match all pathnames within `/` pages
-    '/(fr|en)/:path*'
-  ]
+  matcher: ['/', '/(fr|en)/:path*']
 };
